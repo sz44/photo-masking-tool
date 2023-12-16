7175e69fdf39466862769beaa18aa315837b9379
sz44 <semion44@gmail.com>
2023-12-15 17:42:08 -0800

    scale canvas and image

--- static/client.js
+++ static/client.js
@@ -1,23 +1,31 @@
 const upload = document.querySelector("#upload");
-const prev = document.querySelector("#prev");
 const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
+const MAXWIDTH = 600;
+let scaleFactor = 1;
 
 upload.addEventListener("change", () => {
   if (upload.files.length == 0) return;
   console.log(upload.files[0]);
   const imgsrc = URL.createObjectURL(upload.files[0]);
-  draw(imgsrc);
-  // const el = document.createElement("img");
-  // el.src = imgurl;
-  // prev.appendChild(el);
+  loadImg(imgsrc);
 });
 
-function draw(imgsrc) {
-  const ctx = canvas.getContext("2d");
+function loadImg(imgsrc) {
   const img = new Image();
-  img.onload = () => {
-    ctx.drawImage(img, 0, 0);
-  };
   img.src = imgsrc;
+  
+  img.onload = () => {
+    if (img.height >= MAXWIDTH) {
+      scaleFactor = MAXWIDTH/img.height;
+    }
+    canvas.height = scaleFactor * img.height;
+    canvas.width = scaleFactor * img.width;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    draw();
+  };
+}
+
+function draw() {
 }
